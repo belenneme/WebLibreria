0095eeec15c49d2cdb8c8765da6a55cc4948774a
Belen Neme <belenneme1@gmail.com>
2020-08-08 20:38:56 -0300

    Cambio en el listado de Novedad(nombre) y Visualizacion de Acentos

--- inc_proveedores/inc_proveedores_grid.php
+++ inc_proveedores/inc_proveedores_grid.php
@@ -20,7 +20,7 @@
 	 		<td class="col-xs-1 col-sm-1 col-md-1 col-lg-1"><?php echo $row_proveedor['idproveedor'] ?></td>
 	 		<td class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><?php echo $row_proveedor['razonsocialproveedor'] ?></td>
 	 		<td class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><?php echo $row_proveedor['telefonoproveedor'] ?></td>
-	 		<td class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><?php echo $row_proveedor['nombreprovincia'] ?></td>
+	 		<td class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><?php echo utf8_encode($row_proveedor['nombreprovincia']) ?></td>
 	 		<td class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><?php echo $row_proveedor['nombrelocalidad'] ?></td>
 	 		<td class="col-xs-1 col-sm-1 col-md-1 col-lg-1" ><a href="proveedor_detalle.php?idproveedor=<?php echo $row_proveedor['idproveedor']; ?>"><button type="button" class= "btn btn-warning btn-xs"><span class="glyphicon glyphicon-list"> Detalle</span> </button></a></td>
 	 		<td class="col-xs-1 col-sm-1 col-md-1 col-lg-1"><a href="proveedor_edicion.php?idproveedor=<?php echo $row_proveedor['idproveedor']; ?>"><button type="button" class= "btn btn-info btn-xs"> <span class="glyphicon glyphicon-pencil"> Editar</span> </button></a></td>
