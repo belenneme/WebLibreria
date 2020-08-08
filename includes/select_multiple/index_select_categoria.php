bf42c01e4ea5141561b8d14fd6bb8034687e8760
julieta <julietaaaro@gmail.com>
2020-08-08 20:57:52 -0300

    fixeado buscador de ventas por cliente y guardar un producto sin categoria

--- includes/select_multiple/index_select_categoria.php
+++ includes/select_multiple/index_select_categoria.php
@@ -5,8 +5,8 @@
   $q_categoria=mysql_query($categoria);  
 
 ?>    
-    <select name="idcacategoriaproducto" class="form-control" required="required">
-    <option value="0">Selecciona una categoria</option>
+    <select name="idcacategoriaproducto" class="form-control" required>
+    <option value="">Selecciona una categoria</option>
     <?php 
       while ($row_categoria=mysql_fetch_array($q_categoria)) { ?>
           <option value="<?php echo $row_categoria['idcategoriaproducto'] ?>"><?php echo $row_categoria['nombrecategoria'] ?></option>
