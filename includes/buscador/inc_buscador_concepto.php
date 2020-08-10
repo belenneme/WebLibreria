b533cfd43763635495f9447d72ecfc6da7a1c750
julieta <julietaaaro@gmail.com>
2020-08-09 22:25:49 -0300

    fixed del buscador del concepto por Id o descripcion mas ordenar por idconcepto/codigo

--- includes/buscador/inc_buscador_concepto.php
+++ includes/buscador/inc_buscador_concepto.php
@@ -1,5 +1,5 @@
 <br>
 <form class="form-inline" style="margin-top:0px;" method="get" action="conceptos.php">
-    <input class="input-xlarge form-control" placeholder="Concepto" id="appendedInputButton" type="text" name="busca_concepto">    
+    <input class="input-xlarge form-control" placeholder="Código o Descripción del Concepto" id="appendedInputButton" type="text" name="busca_concepto" style=" WIDTH:260px;">    
     <input type="submit" class="btn btn-info" name="button" id="button" value="Buscar">
 </form>
