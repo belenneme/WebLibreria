fa0b1d2c1266175e043f1d2645a8fb9408410cdf
julieta <julietaaaro@gmail.com>
2020-08-09 22:13:46 -0300

    fixed detalle de empleado con grupo familiar y sin grupo familiar, Modulo empleado sacar ID

--- inc_empleados/inc_empleado_editar_grupofamiliar.php
+++ inc_empleados/inc_empleado_editar_grupofamiliar.php
@@ -10,7 +10,7 @@ $q_grupofamiliar=mysql_query($grupofamiliar);
 
 <table class='table table-bordered table-striped'>
 	<thead>
-		<th>id</th>
+		<!--<th>id</th>-->
 		<th>Apellido</th>
 		<th>Nombre</th>
 		<th>Dni</th>
@@ -21,7 +21,7 @@ $q_grupofamiliar=mysql_query($grupofamiliar);
 	<tbody>
 	<?php while ($row_grupo= mysql_fetch_array($q_grupofamiliar)) { ?>
 		<tr>
-			<td class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><?php echo $row_grupo['idgrupofamiliar']; ?></td>
+			<!--<td class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><?php echo $row_grupo['idgrupofamiliar']; ?></td>-->
 			<td class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><?php echo $row_grupo['apellido_pariente']; ?></td>
 			<td class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><?php echo $row_grupo['nombre_pariente']; ?></td>
 			<td class="col-xs-1 col-sm-1 col-md-1 col-lg-1"><?php echo $row_grupo['dni_pariente']; ?></td>
